7a05c98fe49f939979650815f1303f8120e0a521
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-17 03:20:47 +0900

    [Add] HomeView가 되는 '제주소리'  Tab View 생성

---
+++ DorongDorong/Source/View/JejuSoundView.swift
@@ -0,0 +1,20 @@
+//
+//  JejuSoundView.swift
+//  DorongDorong
+//
+//  Created by geonhyeong on 2022/11/17.
+//
+
+import SwiftUI
+
+struct JejuSoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JejuSoundView_Previews: PreviewProvider {
+    static var previews: some View {
+        JejuSoundView()
+    }
+}
