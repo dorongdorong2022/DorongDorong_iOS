cef0d56e6451763fd55a7fa92db0d0212e03673f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-16 15:16:50 +0900

    [Feat] 프로젝트 시작, .gitignore 설정

---
+++ DorongDorong/DorongDorongApp.swift
@@ -0,0 +1,17 @@
+//
+//  DorongDorongApp.swift
+//  DorongDorong
+//
+//  Created by geonhyeong on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct DorongDorongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
