cd7a1fae884f220c8ecae7fa97880bd28d6e886f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-17 04:41:10 +0900

    [Add] CustomTabBar 파일 추가

---
+++ DorongDorong/Source/View/CustomTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabView.swift
+//  DorongDorong
+//
+//  Created by geonhyeong on 2022/11/17.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+
+}
+
+struct CustomTabView_Previews: PreviewProvider {
+	static var previews: some View {
+		CustomTabView()
+	}
+}
