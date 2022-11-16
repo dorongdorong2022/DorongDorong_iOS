d92a516be8b52682f2733c5e18f12c3c8aa659b2
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-17 05:23:30 +0900

    [Feat] Custom TabBar를 ContentView에 적용

--- DorongDorong/Source/View/ContentView.swift
+++ DorongDorong/Source/View/ContentView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-		
-    }
+	@State private var tabSelection: Tab = .sound
+	
+	var body: some View {
+		ZStack(alignment: .bottom) {
+			switch tabSelection {
+			case .sound:
+				JejuSoundView()
+			case .tale:
+				Color.blue
+			default:
+				Color.red
+			}
+			
+			CustomTabView(selection: $tabSelection)
+		} // ZStack
+		.edgesIgnoringSafeArea(.all)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
