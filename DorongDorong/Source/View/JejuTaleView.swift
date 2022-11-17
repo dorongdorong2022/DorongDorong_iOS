6fbf44e260d5767f31d84d0d6923ecd732d0690a
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-17 11:18:16 +0900

    [Add] 제주 이야기 View 파일 추가

---
+++ DorongDorong/Source/View/JejuTaleView.swift
@@ -0,0 +1,20 @@
+//
+//  JejuTaleView.swift
+//  DorongDorong
+//
+//  Created by geonhyeong on 2022/11/17.
+//
+
+import SwiftUI
+
+struct JejuTaleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JejuTaleView_Previews: PreviewProvider {
+    static var previews: some View {
+        JejuTaleView()
+    }
+}
