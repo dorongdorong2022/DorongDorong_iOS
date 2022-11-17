a9c119df97cc86c34af881224698c3992d2708d9
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-17 14:43:48 +0900

    [Feat] 오디오 진행 Bar View

--- DorongDorong/Source/View/JejuTaleVoiceView.swift
+++ DorongDorong/Source/View/JejuTaleVoiceView.swift
@@ -11,6 +11,15 @@ struct JejuTaleVoiceView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+//MARK: - JejuTaleVoice CellBar View
+struct JejuTaleVoiceCellBarView: View {
+	var isStep: Bool // 색상을 변경할지 결졍
+	
+	var body: some View {
+	  Rectangle()
+		.frame(width: 2, height: 32)
+		.foregroundColor(isStep ? Color("violet") : Color(uiColor: .lightGray))
+	}
 }
 
 struct JejuTaleVoiceView_Previews: PreviewProvider {
