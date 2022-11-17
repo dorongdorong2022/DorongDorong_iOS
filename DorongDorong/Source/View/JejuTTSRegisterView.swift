0487f075fb7592cb056de7cd59fb8722c3061326
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-17 19:57:12 +0900

    [Add] TTS 등록하는 View 파일 추가

---
+++ DorongDorong/Source/View/JejuTTSRegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  JejuTTSRegisterView.swift
+//  DorongDorong
+//
+//  Created by geonhyeong on 2022/11/17.
+//
+
+import SwiftUI
+
+struct JejuTTSRegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JejuTTSRegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        JejuTTSRegisterView()
+    }
+}
