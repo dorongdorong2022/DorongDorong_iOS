7679c4f979d30dfb32988cc4ccbef8dff6ef4b88
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-11-17 11:18:39 +0900

    [Feat] 제주 이야기 View 화면 구성

--- DorongDorong/Source/View/JejuTaleView.swift
+++ DorongDorong/Source/View/JejuTaleView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct JejuTaleView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@Binding var stack: NavigationPath
+	
+	var body: some View {
+		VStack {
+			Image("sample1")
+				.resizable()
+				.scaledToFill()
+		}
+		.navigationBarItems(leading: Button(action: {
+			// Button action
+		}) {
+			Text("설화변경")
+				.font(.system(size: 12))
+				.foregroundColor(.white)
+				.padding(5)
+				.overlay(
+					RoundedRectangle(cornerRadius: 6)
+						.stroke(.white, lineWidth: 1)
+				)
+		}, trailing: Button(action: {
+			
+		}) {
+			Image(systemName: "person.wave.2.fill")
+				.foregroundColor(.white)
+		})
+	}
+
 }
 
 struct JejuTaleView_Previews: PreviewProvider {
     static var previews: some View {
-        JejuTaleView()
+		NavigationStack {
+			ZStack {
+				JejuTaleView(stack: .constant(NavigationPath()))
+			}
+		}
+		.edgesIgnoringSafeArea(.all)
     }
 }
